8ecad67e4683f628e298536133e7a9db1e6c987c
MariiaO28 <idg.masha@gmail.com>
2024-12-01 22:56:53 +0100

    final adjustments to qr code generation

--- src/validation/qr.js
+++ src/validation/qr.js
@@ -1,8 +1,5 @@
 import Joi from 'joi';
 
 export const createQRCodeSchema = Joi.object({
-    codeId: Joi.number().required(),
-    isAssigned: Joi.boolean().default(false),
-    qrCodeImageUrl: Joi.string().allow(null),
-    qrCodeImagePath: Joi.string().allow(null),
-  });
+  count: Joi.number().required(),
+});
