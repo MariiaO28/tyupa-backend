242235296bf6dc99bd4765f453831639d23a6745
novikovanat <72397136+novikovanat@users.noreply.github.com>
2025-01-08 21:14:34 +0100

    wraper updated

--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -1,8 +1,13 @@
+import createHttpError from 'http-errors';
 export const ctrlWrapper = (controller) => {
   return async (req, res, next) => {
     try {
       await controller(req, res, next);
     } catch (error) {
+      if (error.message.includes('Cast to ObjectId')) {
+        next(createHttpError(404, 'Wrong ID format'));
+        return;
+      }
       next(error);
     }
   };
